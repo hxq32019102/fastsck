10328325b811468a65aaee169772cad9551f756e
runoob <test@runoob.com>
2021-01-20 14:46:30 +0800

    修改密码功能

--- routers/user.py
+++ routers/user.py
@@ -3,10 +3,10 @@
 # @Author : hxq
 # @File : user.py
 from fastapi import APIRouter
-from typing import List
-from fastapi import Depends, HTTPException
 from sqlalchemy.orm import Session
-from sql_ import crud, schemas, models
+
+from dependencies import *
+from sql_ import crud, models
 from sql_.database import SessionLocal, engine
 
 models.Base.metadata.create_all(bind=engine)
@@ -21,19 +21,9 @@ def get_db():
         db.close()
 
 
-@router.get("/users/", response_model=List[schemas.User])
-def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
-    users = crud.get_users(db, skip=skip, limit=limit)
+@router.get("/password/change")
+def read_users(username:str,old_password:str,new_password:str,db: Session = Depends(get_db)):
+    '''修改 密码'''
+    users = crud.password_change(db, username=username,password=old_password,new_password=new_password)
     return users
 
-@router.get("/passqord/")
-def read_users(username:str,  db: Session = Depends(get_db)):
-    users = crud.get_password(db, username=username)
-    return users
-
-@router.get("/users/{user_id}", response_model=schemas.User)
-def read_user(user_id: int, db: Session = Depends(get_db)):
-    db_user = crud.get_user(db, user_id=user_id)
-    if db_user is None:
-        raise HTTPException(status_code=404, detail="User not found")
-    return db_user
--- sql_/crud.py
+++ sql_/crud.py
@@ -12,8 +12,16 @@ from sql_ import schemas, models
 
 
 # user
-def get_user(db: Session, user_id: int):
-    return db.query(models.User).filter(models.User.id == user_id).first()
+def password_change(db: Session, username: str, password: str,new_password: str):
+    user = db.query(models.User).filter(models.User.username == username, models.User.password == password).first()
+    if user:
+        user = db.query(models.User).filter(models.User.id==user.id).first()
+        print(user)
+        user.password=new_password
+        db.commit()
+        db.refresh(user)
+        return user
+
 
 
 def get_password(db: Session, username: str):
@@ -42,16 +50,16 @@ def get_dir_by_id(db: Session, id: int):
 
 
 def get_dir_by_name_parent_id(db: Session, dirname: str, parent_id: int):
-    return db.query(models.DirInfo).filter(models.DirInfo.parent_id == parent_id, models.DirInfo.dir_name == dirname).first()
+    return db.query(models.DirInfo).filter(models.DirInfo.parent_id == parent_id,
+                                           models.DirInfo.dir_name == dirname).first()
 
 
 def create_dir(db: Session, dir: schemas.DirCreate):
-    db_dir = models.DirInfo(dir_name=dir.dir_name, parent_id=dir.parent_id, status=1,creat_time=datetime.now())
+    db_dir = models.DirInfo(dir_name=dir.dir_name, parent_id=dir.parent_id, status=1, creat_time=datetime.now())
     db.add(db_dir)
     db.commit()
     db.refresh(db_dir)
     return db_dir
-
 
 
 # file
@@ -59,16 +67,22 @@ def get_file_list(db: Session, dir_id: int):
     return db.query(models.FileInfo).filter(models.FileInfo.dir_id == dir_id)
 
 
-
 def get_file_by_dirid_filename(db: Session, dirid: int, filename: str):
-    return db.query(models.FileInfo).filter(models.FileInfo.dir_id == dirid, models.FileInfo.file_name == filename).first()
+    return db.query(models.FileInfo).filter(models.FileInfo.dir_id == dirid,
+                                            models.FileInfo.file_name == filename).first()
+
 
 def get_file_by_uid(db: Session, uid: str):
     return db.query(models.FileInfo).filter(models.FileInfo.uid == uid).first()
+
+
 def get_file_name_by_url(db: Session, uid: str):
     return db.query(models.FileInfo).filter(models.FileInfo.uid == uid).first()
-def create_file(db: Session,dir_id:int,file_name:str,file_size:int,uid:str):
-    db_file = models.FileInfo(dir_id=dir_id,file_name=file_name,file_size=file_size,file_upload_time=datetime.now(),uid=uid)
+
+
+def create_file(db: Session, dir_id: int, file_name: str, file_size: int, uid: str):
+    db_file = models.FileInfo(dir_id=dir_id, file_name=file_name, file_size=file_size, file_upload_time=datetime.now(),
+                              uid=uid)
     db.add(db_file)
     db.commit()
     db.refresh(db_file)
@@ -80,5 +94,3 @@ def create_file(db: Session,dir_id:int,file_name:str,file_size:int,uid:str):
     # file_upload_time = Column(DateTime, nullable=False)
     # file_status = Column(Integer, nullable=False, server_default=text("'1'"))
     # url = Column(String(255), nullable=False)
-
-
